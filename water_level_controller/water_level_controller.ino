037239be5f8baa6ca9b928fcf15af785c656664a
Christopher Hill <chill@mirabeau.nl>
2020-04-01 18:33:32 +0200

    added countdown

--- water_level_controller/water_level_controller.ino
+++ water_level_controller/water_level_controller.ino
@@ -8,7 +8,9 @@ int Min_Moisture_Level = 300; // Moisture Level of dry soil
 int Max_Moisture_Level = 700; // Moisture Level of wet soil
 int Max_Watering_Time = 10000; // 10 Seconds
 
-
+int Time_Measurement;
+int First_Moisture_Level;
+int Start_Watering_Time;
 
 void setup() {
   // Set up pin modes:
@@ -22,13 +24,11 @@ void setup() {
 }
 
 void loop() {
-  int Time_Measurement = 0;
-  int First_Moisture_Level = 0;
+  
   int Current_Moisture_Level = 0;
-  int Start_Watering_Time = 0;
   int Current_Watering_Time = 0;
 
-  Serial.println("Start of program");
+  // Serial.println("Start of program");
 
   if (digitalRead(Sleep_Led) == LOW) {
     digitalWrite(Sleep_Led, HIGH); // Turn off sleep led
@@ -40,8 +40,9 @@ void loop() {
     digitalWrite(Moisture_Level_Power, HIGH); // Turn on moisture sensor to make a reading
   }
   First_Moisture_Level = analogRead(Moisture_Level_Sensor);
-  Serial.print("First Moisture Level: "); Serial.println(First_Moisture_Level);
+  // Serial.print("First Moisture Level: "); Serial.println(First_Moisture_Level);
   if (First_Moisture_Level <= Min_Moisture_Level) {
+    Serial.println("Plant is dry, watering plant");
     Start_Watering_Time = millis();
     while ((Current_Moisture_Level <= Max_Moisture_Level) && (Current_Watering_Time <= Max_Watering_Time)) {
       Current_Watering_Time = millis() - Start_Watering_Time;
@@ -51,7 +52,7 @@ void loop() {
       if (digitalRead(Motor_Power_Pin) == LOW) {
         digitalWrite(Motor_Power_Pin, HIGH);
       }
-      delay(100);
+      delay(10);
     }
   }
   Current_Moisture_Level = analogRead(Moisture_Level_Sensor);
@@ -62,7 +63,7 @@ void loop() {
     digitalWrite(Motor_Power_Pin, LOW); // Make sure the watering motor is powered down.
   }
   if (First_Moisture_Level <= Min_Moisture_Level) {
-    Serial.println("Plant needed watering");
+    // Serial.println("Plant needed watering");
     Serial.print("Moisture Level change: "); Serial.println(Current_Moisture_Level - First_Moisture_Level);  
     Serial.print("Watering time: "); Serial.println(Current_Watering_Time);
   }
@@ -73,11 +74,19 @@ void loop() {
     Serial.println("Plant is too wet");
   }
 
-  Serial.print("Execute time: "); Serial.println(millis() - Time_Measurement);
+  // Serial.print("Execute time: "); Serial.println(millis() - Time_Measurement);
 
   if (digitalRead(Sleep_Led) == HIGH) {
     digitalWrite(Sleep_Led, LOW); // Turn off sleep led
   }
-
-  delay(5000);  
+  Serial.print("Restarting in: 5");
+  delay(1000);  
+  Serial.print("..4");
+  delay(1000);  
+  Serial.print("..3");
+  delay(1000);  
+  Serial.print("..2");
+  delay(1000);  
+  Serial.println("..1");
+  delay(1000);
 }
